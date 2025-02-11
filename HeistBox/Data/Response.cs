2086cd70172539591ccf664ff27fa354508ca7b8
Justin <jeo961@gmail.com>
2025-02-11 17:18:20 -0500

    added timer

---
+++ HeistBox/Data/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace HeistBox.Data
+{
+    public class Response
+    {
+        public string answer {  get; set; }
+        public PlayerData player { get; set; }
+        public bool wasChosen { get; set; } = false;
+    }
+}
